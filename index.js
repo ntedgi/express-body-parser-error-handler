b10a3ce9125f0159bf7b7226ff6981a1e4e2969c
Abdullahaly <abdullahaly.dev@gmail.com>
2024-07-14 13:14:54 +0300

    pass next function to onError function and fix tests
    I just passed the next function to the onError function.
    
    In the test file, I was getting errors because the error messages were different

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-function isBodyParserError (error) {
+function isBodyParserError(error) {
   const bodyParserCommonErrorsTypes = [
-    'encoding.unsupported',
-    'entity.parse.failed',
-    'entity.verify.failed',
-    'request.aborted',
-    'request.size.invalid',
-    'stream.encoding.set',
-    'parameters.too.many',
-    'charset.unsupported',
-    'entity.too.large'
-  ]
-  return bodyParserCommonErrorsTypes.includes(error.type)
+    "encoding.unsupported",
+    "entity.parse.failed",
+    "entity.verify.failed",
+    "request.aborted",
+    "request.size.invalid",
+    "stream.encoding.set",
+    "parameters.too.many",
+    "charset.unsupported",
+    "entity.too.large",
+  ];
+  return bodyParserCommonErrorsTypes.includes(error.type);
 }
 
-function bodyParserErrorHandler (
-  {
-    // eslint-disable-next-line
-        onError = (err, req, res) => {
-    },
-    errorMessage = (err) => {
-      return `Body Parser failed to parse request --> ${err.message}`
-    }
-  } = {}) {
+function bodyParserErrorHandler({
+  // eslint-disable-next-line
+  onError = (err, req, res) => {},
+  // i think "errorMessage" function is useless now
+  errorMessage = (err) => {
+    return `Body Parser failed to parse request --> ${err.message}`;
+  },
+} = {}) {
   return (err, req, res, next) => {
     if (err && isBodyParserError(err)) {
-      onError(err, req, res)
-      res.status(err.status)
-      res.send({ message: errorMessage(err) })
-    } else next(err)
-  }
+      onError(err, req, res, next);
+
+      if (!res.headersSent) {
+        res.status(err.status);
+        res.send({ message: errorMessage(err) });
+      }
+    } else next(err);
+  };
 }
 
-module.exports = bodyParserErrorHandler
+module.exports = bodyParserErrorHandler;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,180 +1,186 @@
-const express = require('express')
-const { urlencoded, json } = require('body-parser')
-const request = require('supertest')
-const { expect } = require('chai')
+const express = require("express");
+const { urlencoded, json } = require("body-parser");
+const request = require("supertest");
+const { expect } = require("chai");
 
-const bodyParserErrorHandler = require('../index')
+const bodyParserErrorHandler = require("../index");
 
-async function createApp (bodyParserMWConfig) {
-  const app = express()
-  const router = express.Router()
-  router.route('/').get(function (req, res) {
-    return res.json({ goodCall: true })
-  })
-  router.route('/').post(function (req, res) {
-    return res.json({ goodCall: true })
-  })
-  const defaultLimitSize = '250kb'
-  app.use(urlencoded({ extended: false, limit: defaultLimitSize }))
-  app.use('/', json({ limit: defaultLimitSize }))
-  app.use(bodyParserErrorHandler(bodyParserMWConfig))
-  app.use(router)
-  return app
+async function createApp(bodyParserMWConfig) {
+  const app = express();
+  const router = express.Router();
+  router.route("/").get(function (req, res) {
+    return res.json({ goodCall: true });
+  });
+  router.route("/").post(function (req, res) {
+    return res.json({ goodCall: true });
+  });
+  const defaultLimitSize = "250kb";
+  app.use(urlencoded({ extended: false, limit: defaultLimitSize }));
+  app.use("/", json({ limit: defaultLimitSize }));
+  app.use(bodyParserErrorHandler(bodyParserMWConfig));
+  app.use(router);
+  return app;
 }
 
-describe('Body Parser Error Error Handling Middle ware ', function () {
-  describe('should handled on dedicated middleware and return 4xx to client ', function () {
-    let app, testServer
+describe("Body Parser Error Error Handling Middle ware ", function () {
+  describe("should handled on dedicated middleware and return 4xx to client ", function () {
+    let app, testServer;
     beforeAll(function (done) {
-      createApp().then(newApp => {
-        app = newApp
+      createApp().then((newApp) => {
+        app = newApp;
         testServer = app.listen(function (err) {
           if (err) {
-            return done(err)
+            return done(err);
           }
-          done()
-        })
-      })
-    })
+          done();
+        });
+      });
+    });
     afterAll(function (done) {
-      testServer.close(done)
-    })
-    it('Sanity - should send back a JSON object with goodCall set to true', function (done) {
+      testServer.close(done);
+    });
+    it("Sanity - should send back a JSON object with goodCall set to true", function (done) {
       request(app)
-        .get('/')
-        .set('Content-Type', 'application/json')
+        .get("/")
+        .set("Content-Type", "application/json")
         .expect(200, function (err, res) {
           if (err) {
-            return done(err)
+            return done(err);
           }
-          const callStatus = res.body.goodCall
-          expect(callStatus).to.equal(true)
-          done()
-        })
-    })
-    it('Sanity - should catch error inside Body Parser handler when sending broken json', function (done) {
+          const callStatus = res.body.goodCall;
+          expect(callStatus).to.equal(true);
+          done();
+        });
+    });
+    it("Sanity - should catch error inside Body Parser handler when sending broken json", function (done) {
       request(app)
-        .post('/')
-        .set('Content-Type', 'application/json')
-        .send('{ email: \'email\', password: \'password\'')
+        .post("/")
+        .set("Content-Type", "application/json")
+        .send("{ email: 'email', password: 'password'")
         .expect(400, function (err, res) {
           if (err) {
-            return done(err)
+            return done(err);
           }
-          expect(JSON.parse(res.text).message).to.equal(
-            'Body Parser failed to parse request --> Unexpected token e in JSON at position 2'
-          )
 
-          done()
-        })
-    })
-    it('Sanity - should catch error inside Body Parser handler when sending 251Kb payload', function (done) {
-      const KB = 1024
-      const limit = 257 * KB
-      const longStr = new Array(limit + 1).join('*')
+          expect(JSON.parse(res.text).message).oneOf([
+            "Body Parser failed to parse request --> Unexpected token e in JSON at position 2",
+            "Body Parser failed to parse request --> Expected property name or '}' in JSON at position 2",
+          ]);
+
+          done();
+        });
+    });
+    it("Sanity - should catch error inside Body Parser handler when sending 251Kb payload", function (done) {
+      const KB = 1024;
+      const limit = 257 * KB;
+      const longStr = new Array(limit + 1).join("*");
 
       request(app)
-        .post('/')
-        .set('Content-Type', 'application/json')
+        .post("/")
+        .set("Content-Type", "application/json")
         .send(`{ email:${longStr}}`)
         .expect(413, function (err, res) {
           if (err) {
-            return done(err)
+            return done(err);
           }
 
-          expect(JSON.parse(res.text).message).to.equal('Body Parser failed to parse request --> request entity too large')
+          expect(JSON.parse(res.text).message).to.equal(
+            "Body Parser failed to parse request --> request entity too large"
+          );
 
-          done()
-        })
-    })
-  })
-  describe('parameters', function () {
-    it('should support onError call back function', function (done) {
-      let onErrorFunctionCalled = false
+          done();
+        });
+    });
+  });
+  describe("parameters", function () {
+    it("should support onError call back function", function (done) {
+      let onErrorFunctionCalled = false;
 
-      function onErrorFunction () {
-        onErrorFunctionCalled = true
+      function onErrorFunction(error, req, res) {
+        onErrorFunctionCalled = true;
       }
 
-      createApp({ onError: onErrorFunction }).then(app => {
+      createApp({ onError: onErrorFunction }).then((app) => {
         request(app)
-          .post('/')
-          .set('Content-Type', 'application/json')
-          .send('{ email: \'email\', password: \'password\'')
+          .post("/")
+          .set("Content-Type", "application/json")
+          .send("{ email: 'email', password: 'password'")
           .expect(400, function (err, res) {
-            expect(JSON.parse(res.text).message).to.equal(
-              'Body Parser failed to parse request --> Unexpected token e in JSON at position 2'
-            )
-            onErrorFunctionCalled ? done() : done(err)
-          })
-      })
-    })
-    it('should support custom errorMessage  function', function (done) {
+            expect(JSON.parse(res.text).message).oneOf([
+              "Body Parser failed to parse request --> Unexpected token e in JSON at position 2",
+              "Body Parser failed to parse request --> Expected property name or '}' in JSON at position 2",
+            ]);
+
+            onErrorFunctionCalled ? done() : done(err);
+          });
+      });
+    });
+    it("should support custom errorMessage  function", function (done) {
       const errorMessageExample = () => {
-        return 'what does the fox say?'
-      }
-      createApp({ errorMessage: errorMessageExample }).then(app => {
+        return "what does the fox say?";
+      };
+      createApp({ errorMessage: errorMessageExample }).then((app) => {
         request(app)
-          .post('/')
-          .set('Content-Type', 'application/json')
-          .send('{ email: \'email\', password: \'password\'')
+          .post("/")
+          .set("Content-Type", "application/json")
+          .send("{ email: 'email', password: 'password'")
           .expect(400, function (err, res) {
             expect(JSON.parse(res.text).message).to.equal(
               errorMessageExample(err)
-            )
-            done()
-          })
-      })
-    })
-    it('should pass error not related to body parser ', function (done) {
-      const app = express()
-      const router = express.Router()
-      router.route('/').post(function (req, res) {
-        return res.json({ goodCall: true })
-      })
-      app.use('/', json({ limit: '250kb' }))
+            );
+            done();
+          });
+      });
+    });
+    it("should pass error not related to body parser ", function (done) {
+      const app = express();
+      const router = express.Router();
+      router.route("/").post(function (req, res) {
+        return res.json({ goodCall: true });
+      });
+      app.use("/", json({ limit: "250kb" }));
       app.use((e, req, res, next) => {
-        throw new Error("I'm a bad error")
-      })
-      app.use(bodyParserErrorHandler({}))
-      app.use(router)
+        throw new Error("I'm a bad error");
+      });
+      app.use(bodyParserErrorHandler({}));
+      app.use(router);
       request(app)
-        .post('/')
-        .set('Content-Type', 'application/json')
-        .send('{ email: \'email\', password: \'password\'}')
+        .post("/")
+        .set("Content-Type", "application/json")
+        .send("{ email: 'email', password: 'password'}")
         .expect(500, function (e, res) {
-          done()
-        })
-    })
-  })
+          done();
+        });
+    });
+  });
 
-  describe('play nice with other middlewares', function () {
-    it('should call next if no errors found', function (done) {
-      let middleWareCalled = false
+  describe("play nice with other middlewares", function () {
+    it("should call next if no errors found", function (done) {
+      let middleWareCalled = false;
 
-      function nextMiddleWare () {
+      function nextMiddleWare() {
         return (req, res, next) => {
-          middleWareCalled = true
-          next()
-        }
+          middleWareCalled = true;
+          next();
+        };
       }
 
-      const app = express()
-      const router = express.Router()
-      router.route('/').post(function (req, res) {
-        return res.json({ goodCall: true })
-      })
-      app.use(bodyParserErrorHandler({}))
-      app.use(nextMiddleWare())
-      app.use(router)
+      const app = express();
+      const router = express.Router();
+      router.route("/").post(function (req, res) {
+        return res.json({ goodCall: true });
+      });
+      app.use(bodyParserErrorHandler({}));
+      app.use(nextMiddleWare());
+      app.use(router);
       request(app)
-        .post('/')
-        .set('Content-Type', 'application/json')
-        .send('{}')
+        .post("/")
+        .set("Content-Type", "application/json")
+        .send("{}")
         .expect(200, function (e, res) {
-          expect(middleWareCalled).to.equal(true)
-          done()
-        })
-    })
-  })
-})
+          expect(middleWareCalled).to.equal(true);
+          done();
+        });
+    });
+  });
+});
